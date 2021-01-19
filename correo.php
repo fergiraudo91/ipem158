33b29d1d7021444e7dc5763b120714fde4995002
Fernando Giraudo <fergiraudo91@gmail.com>
2021-01-19 10:50:21 -0300

    Detalles finales

--- correo.php
+++ correo.php
@@ -1,20 +1,15 @@
 <?php
-echo "<h1>".$_POST['nombre']."</h1>";
-    if(isset($_POST['nombre']) && isset($_POST['apellido']) && isset($_POST['email']) && isset($_POST['mensaje'])){
-        if(!empty($_POST['nombre']) && !empty($_POST['apellido']) && !empty($_POST['email']) && !empty($_POST['mensaje'])){
-            echo "Hola entre";
-            $name = $_POST['nombre'];
-            $apellido = $_POST['apellido'];
-            $email = $_POST['email'];
-            $mensaje = $_POST['mensaje'];
-            $header = "From:" . $mail . "\r\n";
-            $header .= "X-Mailer: PHP/" . phpversion();
-            $header .= "Reply to: ipem158leopoldolugones@hotmail.com.ar" . "\r\n";
-            $mail = mail($name . " " . $apellido, "Enviado desde sitio web", $mensaje, $header);
-            echo $mail;
-            if($mail){
-                echo "<h4>Mail enviado exitosamente</h4>";
-            }
+    if(isset($_POST["nombre"]) && isset($_POST["apellido"]) && isset($_POST["email"]) && isset($_POST["mensaje"]) ){
+        $to = "fergiraudo91@gmail.com";
+        $subject = "Mensaje Enviado desde sitio web";
+        $contenido .= "Nombre: ".$_POST["nombre"]. " " . $_POST['apellido'] . "\n";
+        $contenido .= "Email: ".$_POST["email"]."\n\n";
+        $contenido .= "Comentario: ".$_POST["mensaje"]."\n\n";
+        $header = "From: admin@ipem158lugones.com.ar\nReply-To:".$_POST["email"]."\n";
+        $header .= "Mime-Version: 1.0\n";
+        $header .= "Content-Type: text/plain";
+        if(mail($to, $subject, $contenido ,$header)){
+            echo "Mail Enviado.";
         }
     }
 ?>
