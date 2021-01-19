d9b2aa44de53e87242a7cd4a332e2d9fdfc8a072
Fernando Giraudo <fergiraudo91@gmail.com>
2021-01-19 12:23:38 -0300

    Modificaciones a archivos php

--- correo.php
+++ correo.php
@@ -1,15 +1,56 @@
 <?php
-    if(isset($_POST["nombre"]) && isset($_POST["apellido"]) && isset($_POST["email"]) && isset($_POST["mensaje"]) ){
-        $to = "fergiraudo91@gmail.com";
-        $subject = "Mensaje Enviado desde sitio web";
-        $contenido .= "Nombre: ".$_POST["nombre"]. " " . $_POST['apellido'] . "\n";
-        $contenido .= "Email: ".$_POST["email"]."\n\n";
-        $contenido .= "Comentario: ".$_POST["mensaje"]."\n\n";
-        $header = "From: admin@ipem158lugones.com.ar\nReply-To:".$_POST["email"]."\n";
-        $header .= "Mime-Version: 1.0\n";
-        $header .= "Content-Type: text/plain";
-        if(mail($to, $subject, $contenido ,$header)){
-            echo "Mail Enviado.";
-        }
-    }
-?>
+/**
+ * @version 1.0
+ */
+
+require("class.phpmailer.php");
+require("class.smtp.php");
+
+// Valores enviados desde el formulario
+if ( !isset($_POST["nombre"]) || !isset($_POST["apellido"]) || !isset($_POST["email"]) || !isset($_POST["mensaje"]) ) {
+    die ("Es necesario completar todos los datos del formulario");
+}
+$nombre = $_POST["nombre"] . " " . $_POST['apellido'];
+$email = $_POST["email"];
+$mensaje = $_POST["mensaje"];
+
+// Datos de la cuenta de correo utilizada para enviar vía SMTP
+$smtpHost = "l2000409.ferozo.com";  // Dominio alternativo brindado en el email de alta 
+$smtpUsuario = "admin@ipem158lugones.com.ar";  // Mi cuenta de correo
+$smtpClave = "Fer07151405";  // Mi contraseña
+
+// Email donde se enviaran los datos cargados en el formulario de contacto
+$emailDestino = "ipem158leopoldolugones@hotmail.com.ar";
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->Port = 465; 
+$mail->SMTPSecure = 'ssl';
+$mail->IsHTML(true); 
+$mail->CharSet = "utf-8";
+
+
+// VALORES A MODIFICAR //
+$mail->Host = $smtpHost; 
+$mail->Username = $smtpUsuario; 
+$mail->Password = $smtpClave;
+
+$mail->From = $email; // Email desde donde envío el correo.
+$mail->FromName = $nombre;
+$mail->AddAddress($emailDestino); // Esta es la dirección a donde enviamos los datos del formulario
+
+$mail->Subject = "Sitio web, formulario de contacto"; // Este es el titulo del email.
+$mensajeHtml = nl2br($mensaje);
+$mail->Body = "{$mensajeHtml} <br /><br />Formulario de sitio web. By Fernando Giraudo<br />"; // Texto del email en formato HTML
+$mail->AltBody = "{$mensaje} \n\n Formulario de ejemplo By Fernando Giraudo"; // Texto sin formato HTML
+// FIN - VALORES A MODIFICAR //
+
+$estadoEnvio = $mail->Send(); 
+if($estadoEnvio){
+    echo "<h1 style='text-align: center'>El correo fue enviado correctamente.</h1>";
+    echo "<a href='http://ipem158lugones.com.ar'><h1 style='text-align: center'>Volver a la pagina principal</h1></a>";
+} else {
+    echo "<h1 style='text-align: center'>Ocurrió un error inesperado.</h1>";
+    echo "<a href='http://ipem158lugones.com.ar'><h1 style='text-align: center'>Volver a la pagina principal</h1></a>";
+}
